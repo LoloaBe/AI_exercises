6bc886534dece79d07b653c24f24fc9be30a0dbc
LoloaBe <luca@lucacriscuolo.com>
2024-10-08 21:12:55 +0000

    try this

---
+++ reader/temp_cached_article.py
@@ -0,0 +1,1 @@
+article_text = """The ancient Greeks made the town of Metaponto to protect Sybaris from Taranto. There is a nice 1 km long beach with white sand that people like to visit. The Greek settlers from Achaia made the city of Metaponto in the second part of the 7th century to stay safe from Taranto getting bigger."""
